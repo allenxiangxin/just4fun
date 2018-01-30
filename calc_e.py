2f11b6790129718ed425a56d62f9131d82043318
Xin Xiang <xinxiang@nat-oitwireless-inside-vapornet100-c-11117.Princeton.EDU>
2018-01-29 23:12:20 -0500

    add calc_e.py

---
+++ calc_e.py
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+#How to calculate e using Monte Carlo
+import random
+random.uniform(0, 1)
+
+N=1000000
+sum_n = 0.;
+for i in range(N):
+    n=0
+    s=0.
+    while s<=1.:
+        s=s+random.random()
+        n = n+1
+    sum_n = sum_n+ n;
+print(sum_n/N) #the average
+
